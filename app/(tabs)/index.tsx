2be339124efaa043898209d36efdccddc49388bd
alfonso <alfonsolasaguirre@gmail.com>
2024-10-26 13:13:15 +0200

    step(1): buttons animations

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,109 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { useState } from 'react';
+import { Text, TouchableOpacity, View, ViewStyle } from 'react-native';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import Animted, {
+  FadeInLeft,
+  FadeOutLeft,
+  LinearTransition,
+} from 'react-native-reanimated';
+import Animated from 'react-native-reanimated';
 
-export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+const AnimatedTouchable = Animated.createAnimatedComponent(TouchableOpacity);
+
+const _buttonHeight = 50;
+
+interface AnimatedButtonProps extends React.PropsWithChildren {
+  style: ViewStyle;
+  onPress: () => void;
 }
 
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+const AnimatedButton: React.FC<AnimatedButtonProps> = ({
+  style,
+  onPress,
+  children,
+}) => {
+  return (
+    <AnimatedTouchable
+      onPress={onPress}
+      style={[
+        {
+          height: _buttonHeight,
+          width: 150,
+          justifyContent: 'center',
+          alignItems: 'center',
+          borderRadius: 230,
+        },
+        style,
+      ]}
+      entering={FadeInLeft.springify().stiffness(200).damping(18)}
+      exiting={FadeOutLeft.springify().stiffness(200).damping(18)}
+      layout={LinearTransition.springify().stiffness(200).damping(18)}
+    >
+      {children}
+    </AnimatedTouchable>
+  );
+};
+
+const Data = [
+  {
+    step: 1,
+    title: 'Título 1',
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  {
+    step: 2,
+    title: 'Título 2',
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  {
+    step: 3,
+    title: 'Título 3',
   },
-});
+];
+export default function HomeScreen() {
+  const [seletedStep, setSeletedStep] = useState(0);
+
+  const isBackButtonVisible = seletedStep > 0;
+
+  return (
+    <Animted.View
+      style={{ flex: 1, justifyContent: 'center', paddingBottom: 30 }}
+    >
+      <Text>{Data[seletedStep].title}</Text>
+      <View>
+        <View style={{ flexDirection: 'row', gap: 16 }}>
+          {isBackButtonVisible && (
+            <AnimatedButton
+              style={{ backgroundColor: '#ddd' }}
+              onPress={() => {
+                setSeletedStep(seletedStep - 1);
+              }}
+            >
+              <Animated.Text
+                layout={LinearTransition.springify()
+                  .stiffness(2000)
+                  .damping(18)}
+              >
+                Anterior
+              </Animated.Text>
+            </AnimatedButton>
+          )}
+          <AnimatedButton
+            style={{ backgroundColor: '#d2d', flex: 1 }}
+            onPress={() => {
+              if (seletedStep === Data.length - 1) {
+                return;
+              }
+
+              setSeletedStep(seletedStep + 1);
+            }}
+          >
+            <Animated.Text
+              layout={LinearTransition.springify().stiffness(200).damping(18)}
+            >
+              Continuar
+            </Animated.Text>
+          </AnimatedButton>
+        </View>
+      </View>
+    </Animted.View>
+  );
+}
